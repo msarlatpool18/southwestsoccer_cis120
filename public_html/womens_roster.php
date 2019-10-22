5eaa5a942c7675e84baa6b638dbce3686d1274fa
Micah Sarlat-Pool <micah.sarlatpool@my.simpson.edu>
2019-10-22 15:33:35 -0500

    html to php

--- public_html/womens_roster.php
+++ public_html/womens_roster.php
@@ -14,20 +14,20 @@ Southwest College Soccer
 </header>
 <nav class="nav_bar shadow" >
     <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="college_information.html">Southwest College Information</a></li>
-        <li><a href="conference_information.html">ARC Conference Information</a></li>
-        <li class="drop_down"> <a href="" class="active">Rosters:</a>
+        <li><a href="index.php" class="active">Home</a></li>
+        <li><a href="college_information.php">Southwest College Information</a></li>
+        <li><a href="conference_information.php">ARC Conference Information</a></li>
+        <li class="drop_down"> <a href="">Rosters:</a>
             <ul class="drop_down_content">
-                <a href="womens_roster.html" class="drop active">Women's Roster</a>
-                 <a href="mens_roster.html" class="drop">Men's Roster</a>
+                <a href="womens_roster.php" class="drop">Women's Roster</a>
+                 <a href="mens_roster.php" class="drop">Men's Roster</a>
             </ul></li>
             <div class="drop2">
-            <li><a href="womens_roster.html">Women's Roster</a></li>
-            <li><a href="mens_roster.html">Men's Roster</a></li>
+            <li><a href="womens_roster.php">Women's Roster</a></li>
+            <li><a href="mens_roster.php">Men's Roster</a></li>
             </div>
-        <li><a href="team_schedules.html">Team Schedules</a></li>
-        <li><a href="team_store.html">Team Store</a></li>
+        <li><a href="team_schedules.php">Team Schedules</a></li>
+        <li><a href="team_store.php">Team Store</a></li>
     </ul>
 </nav>
           
