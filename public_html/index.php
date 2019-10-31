2f1488ad93423eff78b6c19fa4dcb3b39b764fcb
Micah <micah.sarlatpool@my.simpson.edu>
2019-10-31 11:52:07 -0500

    Nav.php

--- public_html/index.php
+++ public_html/index.php
@@ -9,28 +9,7 @@
 </head>
 
 <body>
-<header>
-Southwest College Soccer
-</header>
-
-<nav class="nav_bar shadow" >
-    <ul>
-        <li><a href="index.php" class="active">Home</a></li>
-        <li><a href="college_information.php">Southwest College Information</a></li>
-        <li><a href="conference_information.php">ARC Conference Information</a></li>
-        <li class="drop_down"> <a href="">Rosters:</a>
-            <ul class="drop_down_content">
-                <a href="womens_roster.php" class="drop">Women's Roster</a>
-                 <a href="mens_roster.php" class="drop">Men's Roster</a>
-            </ul></li>
-            <div class="drop2">
-            <li><a href="womens_roster.php">Women's Roster</a></li>
-            <li><a href="mens_roster.php">Men's Roster</a></li>
-            </div>
-        <li><a href="team_schedules.php">Team Schedules</a></li>
-        <li><a href="team_store.php">Team Store</a></li>
-    </ul>
-</nav>
+<?php include 'navigation.php'; ?>
           
 <article class="light_shadow">
 <h2>Welcome!</h2>
